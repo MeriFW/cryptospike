18b1a0ffd29991e49b71c37df2ec77c7c1ea2c12
Meri <meri.calvo@flywire.com>
2017-12-28 13:53:25 +0100

    This encrypts and signs fine

--- script.rb
+++ script.rb
@@ -1,8 +1,7 @@
 require 'gpgme'
 
 GPGME::Key.import(File.open("keys.asc"))
-#encrypted = GPGME::Crypto.encrypt "Hello", :always_trust => true
-encrypted = GPGME::Crypto.encrypt "Hello", :sign => true, :signers => "filer_test@peertransfer.example.com", :always_trust => true
+encrypted = GPGME::Crypto.encrypt "Hello", :sign => true, :always_trust => true
 
 file = File.open("encrypted_file.gpg", "w")
 file.write(encrypted)
